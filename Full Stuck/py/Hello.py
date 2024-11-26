36a8ecee8af19d1c866d2ccc6494f8b24b06102e
Reflak3 <goshurik@gmail.com>
2024-11-26 14:17:25 +0200

    Add files via upload

---
+++ Full Stuck/py/Hello.py
@@ -0,0 +1,19 @@
+import math
+import py_compile
+a=2
+b=0.5
+c=2
+x=a+b+c
+y=a*b*c
+if x>y:
+    print(x)
+else:
+    print(y)
+
+ADAM = int(input("How much apples you ate?"))
+EVE = int(input("Why did you eat the apple?"))
+
+if ADAM and EVE >= 0:
+    print("Blody Bustard!")
+else:
+    print("ARE YOU LIYING TO GOD???")
